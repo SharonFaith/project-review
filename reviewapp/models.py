05340fbf3c656305d38b364b03f295299ccf0d0e
SharonFaith <sharonfaith15@gmail.com>
2020-12-01 01:25:00 +0300

    import relevant modules

--- reviewapp/models.py
+++ reviewapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
+import cloudinary 
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
