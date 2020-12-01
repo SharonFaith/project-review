9c35164f826c7407171a126bece1d2c3ba31539e
SharonFaith <sharonfaith15@gmail.com>
2020-12-01 04:16:53 +0300

    add api paths

--- reviewapp/urls.py
+++ reviewapp/urls.py
@@ -2,11 +2,16 @@ from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import welcome, index
-from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     path('', welcome, name='welcome'),
     path('landing-page/', index, name='index'),
+    path('api/projects/', views.ProjectList.as_view()),
+    path('api/profiles/', views.ProfileList.as_view()),
+    re_path(r'api/projects/project-id/(?P<pk>[0-9]+)/', views.ProjectDescription.as_view()),
+    re_path(r'api/profiles/profile-id/(?P<pk>[0-9]+)/', views.ProfileDescription.as_view()),
+
     
 ]
 if settings.DEBUG:
