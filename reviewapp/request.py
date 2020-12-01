9d9d7157306490fc49f6ef7e2f41d7551b14c66b
SharonFaith <sharonfaith15@gmail.com>
2020-12-01 05:34:00 +0300

    add functions to get data from api

--- reviewapp/request.py
+++ reviewapp/request.py
@@ -46,3 +46,51 @@ def process_projects(project_list):
         project_results.insert(0, project_object)
 
     return project_results
+
+
+def get_profiles():
+
+    get_profiles_url = 'https://app-m-tribune.herokuapp.com/api/profiles/'
+
+    
+    
+    with urllib.request.urlopen(get_profiles_url) as url:
+
+        get_profile_data = url.read()
+        print('hi')
+       
+        #print(get_merch_data)
+        get_profile_response = json.loads(get_profile_data)
+        #print(get_merch_response)
+        profile_results = None
+
+        if get_profile_response != None:
+            profile_results_list = get_profile_response
+            #print(merch_results_list)
+            profile_results = process_profile(profile_results_list)
+
+    return profile_results
+
+def get_projects():
+
+    get_projects_url = 'https://app-m-tribune.herokuapp.com/api/projects/'
+
+    
+    
+    with urllib.request.urlopen(get_projects_url) as url:
+
+        get_projects_data = url.read()
+        print('hi')
+       
+        #print(get_merch_data)
+        get_project_response = json.loads(get_projects_data)
+        #print(get_merch_response)
+        project_results = None
+
+        if get_project_response != None:
+            project_results_list = get_project_response
+            #print(merch_results_list)
+            project_results = process_projects(project_results_list)
+
+    return project_results
+
