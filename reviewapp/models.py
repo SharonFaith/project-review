6e6eff5e9dab56f74aa9cc37cf22f7f7072eede2
SharonFaith <sharonfaith15@gmail.com>
2020-12-03 03:52:36 +0300

    add tests for profile

--- reviewapp/models.py
+++ reviewapp/models.py
@@ -24,6 +24,17 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def update_profile(cls, id, updates):
+        to_update = cls.objects.filter(id = id)
+        to_update.update(bio = updates)
+
 
 class Projects(models.Model):
     profile= models.ForeignKey(Profile, on_delete=models.CASCADE, default = None, related_name='projects')
--- reviewapp/tests.py
+++ reviewapp/tests.py
@@ -1,3 +1,52 @@
 from django.test import TestCase
+from .models import Projects, Profile, Rating, DisplayProfile, DisplayProjects 
+import datetime as dt
+from django.contrib.auth.models import User
+
+
+class ProfileTestClass(TestCase):
+
+    #set up method
+    def setUp(self):
+        self.user1 = User(username = 'a-user')
+        self.user1.save()
+        #self.new = Profile(bio = 'this is a bio', user = self.user1)
+        self.new = Profile(user=self.user1, bio="hi")
+  #  def tearDown(self):
+    #    Profile.objects.all().delete()
+        
+    
+    def test_instance(self):
+
+        self.assertTrue(isinstance(self.new, Profile))
+    
+    def test_save_profile(self):
+
+        self.new.save_profile()
+        profiles = Profile.objects.all()
+
+        self.assertTrue(len(profiles) > 0)
+
+    def test_delete_profile(self):
+        self.new.save_profile()
+        new2 = Profile(user = self.user1, bio = 'profile bio number 2 ')
+        new2.save_profile()
+
+        new2.delete_profile()
+        
+        profiles = Profile.objects.all()
+
+        self.assertTrue(len(profiles) == 1)
+
+    def test_update_profile(self):
+        self.new.save_profile()
+        print(self.new.id)
+        new2 = Profile(user = self.user1, bio = 'profile bio number 2 ')
+        new2.save_profile()
+
+        Profile.update_profile(4, 'the new bio')
+       
+        self.assertEqual(Profile.objects.filter(id = 4).first().bio, 'the new bio')
+
 
 # Create your tests here.
